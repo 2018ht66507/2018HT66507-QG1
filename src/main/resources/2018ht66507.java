e2e8a44dd95edf0e9212bf61154e2a7e3bc04a83
2018ht66507 <57030401+2018ht66507@users.noreply.github.com>
2020-12-15 23:29:14 +0800

    Create 2018ht66507.java

---
+++ src/main/resources/2018ht66507.java
@@ -0,0 +1,21 @@
+package com.2018ht66507;
+
+public class 2018ht66507 {
+
+    public String getMessage(String name) {
+
+        StringBuilder result = new StringBuilder();
+
+        if (name == null || name.trim().length() == 0) {
+
+            result.append(" WILP - Project");
+
+        } else {
+
+            result.append("BITS " + name);
+
+        }
+        return result.toString();
+    }
+
+}
